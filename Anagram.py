cf7dc931369fb37fe37bd63603a63da8c0e96d14
simplemuddySkibidy <nkarybekov228@gmail.com>
2026-04-14 08:24:40 +0600

    Делаем проверку через sorted(), сортируем все символы по порядку и сравниваем

--- Anagram.py
+++ Anagram.py
@@ -3,3 +3,7 @@ s2 = input("Введите вторую строку: ")
 def is_anagram(s1, s2):
     s1 = s1.replace(" ", "").lower()
     s2 = s2.replace(" ", "").lower()
+    if sorted(s1) == sorted(s2):
+        print("Строки являются анаграммами.")
+    else:
+        print("Строки не являются анаграммами.")
