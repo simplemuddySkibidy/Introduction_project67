666fe4384a9d3a45bbfb03aa21ba5f82426851cc
simplemuddySkibidy <nkarybekov228@gmail.com>
2026-04-14 10:11:01 +0600

    Delete Anagram.py

--- Anagram.py
+++
@@ -1,9 +0,0 @@
-s1 = input("Введите первую строку: ")
-s2 = input("Введите вторую строку: ")
-def is_anagram(s1, s2):
-    s1 = s1.replace(" ", "").lower()
-    s2 = s2.replace(" ", "").lower()
-    if sorted(s1) == sorted(s2):
-        print("Строки являются анаграммами.")
-    else:
-        print("Строки не являются анаграммами.")
